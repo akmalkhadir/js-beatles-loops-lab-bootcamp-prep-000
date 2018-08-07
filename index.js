a625eb600acddb7208e07fbb1c868060da46b713
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-08-07 17:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var array = [];
   var arrayLength = array.length;
-for (i = 0; i < arrayLength ; i++){
+for (var i = 0; i < arrayLength ; i++){
   array.push(`${musicians(i)} plays ${instruments}`)
 }
   return array
