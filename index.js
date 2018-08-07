32253604616365a63dd51f2ebda2c2ec0dfd2c2c
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-08-07 18:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var array = [];
-  var arrayLength = musicians.length;
-for (var i = 0; i < arrayLength; i++){
+for (var i = 0; i < musicians.length; i++){
   array.push(`${musicians[i]} plays ${instruments[i]}`)
 }
   return array
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i <= facts.length){
+    facts.
+  }
+  
 }
